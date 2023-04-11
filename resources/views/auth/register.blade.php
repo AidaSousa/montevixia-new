78c70f1d4ede186caf5f6d0281fc826d783f2cf5
Ana <117080170+anavgg@users.noreply.github.com>
2023-04-11 09:39:28 +0200

    changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -31,7 +31,7 @@
     <form method="POST" action="/register" class="form-horizontal ">
   @csrf
   <div class="form-group">
-    <label for="name" class="col-md-4 control-label">Nombre</label>
+    <label for="name" class="col-md-4 mt-3 control-label">Nombre</label>
     <div class="col-md-8">
       <input type="text" name="name" class="form-control" required>
     </div>
