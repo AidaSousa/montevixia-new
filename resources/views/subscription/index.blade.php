c647688a4241757964ea733483a80917dab5d249
nrilova <116728539+nrilova@users.noreply.github.com>
2023-04-13 10:36:58 +0200

    update stripe

--- resources/views/subscription/index.blade.php
+++ resources/views/subscription/index.blade.php
@@ -4,29 +4,40 @@
     <title>Formulario de Suscripción</title>
     <!-- Agrega el script de Stripe.js en la cabecera de tu página -->
     <script src="https://js.stripe.com/v3/"></script>
+    <!-- Agrega los estilos de Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <h1>Formulario de Suscripción</h1>
-    <form action="{{ route('subscription.subscribe') }}" method="post" id="subscription-form">
-        <!-- Contenido del formulario -->
-        <label for="card-holder-name">Nombre del titular de la tarjeta</label>
-        <input type="text" id="card-holder-name" name="card-holder-name" required>
-        <br>
-        <label for="card-number">Número de tarjeta</label>
-        <input type="text" id="card-number" name="card-number" required>
-        <br>
-        <label for="expiry-month">Mes de vencimiento</label>
-        <input type="text" id="expiry-month" name="expiry-month" required>
-        <br>
-        <label for="expiry-year">Año de vencimiento</label>
-        <input type="text" id="expiry-year" name="expiry-year" required>
-        <br>
-        <label for="cvc">CVC</label>
-        <input type="text" id="cvc" name="cvc" required>
-        <br>
-        @csrf
-        <button type="submit">Enviar</button>
-    </form>
+<div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <h1 class="text-center mb-4">Formulario de Suscripción</h1>
+                <form action="{{ route('subscription.subscribe') }}" method="post" id="subscription-form">
+                    <!-- Contenido del formulario -->
+                    <div class="form-group">
+                        <label for="card-holder-name">Nombre del titular de la tarjeta</label>
+                        <input type="text" id="card-holder-name" name="card-holder-name" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="card-number">Número de tarjeta</label>
+                        <input type="text" id="card-number" name="card-number" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="expiry-month">Mes de vencimiento</label>
+                        <input type="text" id="expiry-month" name="expiry-month" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="expiry-year">Año de vencimiento</label>
+                        <input type="text" id="expiry-year" name="expiry-year" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="cvc">CVC</label>
+                        <input type="text" id="cvc" name="cvc" class="form-control" required>
+                    </div>
+                    @csrf
+                    <button type="submit" class="btn btn-secondary" style="background-color: #009284; border: 1px solid #009284;">Enviar</button>
+                    <a href="/profile" class="btn btn-primary" style="background-color: #000000; border-color: #000000;">Volver</a>
+                </form>
     <script>
     // Importa la librería de Stripe.js y configura tu clave pública
     var stripe = Stripe(pk_test_51Mtn5ZCNGUc1Amzp3fRKmWtIqppccf4KyGsPfvzyBAmftNMjuHhb1TBoUl1asEJaJvbEXm0Kt3JC2TlL2e3Rn3CH00QALXOWAH);
