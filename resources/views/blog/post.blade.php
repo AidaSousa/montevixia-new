171bc8d64b4eab80ac001b41c7afb567d4d177b1
Ana <117080170+anavgg@users.noreply.github.com>
2023-04-13 23:42:09 +0200

    update

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -4,8 +4,8 @@
 <div class="container mt-4"> <!-- Contenedor principal con margen superior -->
     <div class="row"> <!-- Fila para el contenido -->
         <div class="col-md-8 offset-md-2"> <!-- Columna principal centrada -->
-            <h1 class="mb-4">{{ $post->title }}</h1> <!-- Título del post con margen inferior -->
-            <p class="mb-4">{{ $post->content }}</p> <!-- Contenido del post con margen inferior -->
+            <h1 class="mb-4 text-justify">{{ $post->title }}</h1> <!-- Título del post con margen inferior -->
+            <p class="mb-4 text-justify">{{ $post->content }}</p> <!-- Contenido del post con margen inferior -->
             <img src="{{ asset($post->image_path) }}" alt="Imagen del post" class="img-fluid mb-4"> <!-- Imagen del post con margen inferior -->
 
             <!-- Botón de regreso -->
