bdb32cd1bda0914cb75c3a75f0c155db221d52f3
Ana <117080170+anavgg@users.noreply.github.com>
2023-04-18 12:50:19 +0200

    navbar

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -103,20 +103,21 @@
 		</div>
 		</div>
 
+
 		<!-- Header Mobile -->
 		<div class="wrap-header-mobile">
-			<!-- Logo moblie -->
+			<!-- Logo moblie -->		
 			<div class="logo-mobile">
-				<a href="index.html"><img src="img/anpa.png" alt="IMG-LOGO"></a>
+				<a href="/"><img src="{{ URL::asset('img/anpa.png') }}" alt="IMG-LOGO"></a>
 			</div>
 
 			<!-- Icon header -->
-			<div class="wrap-icon-header flex-w flex-r-m h-full m-r-15">
-				<!-- <div class="flex-c-m h-full p-r-10">
-				<div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 js-show-modal-search">
+
+			<!-- <div class="wrap-icon-header flex-w flex-r-m m-r-15">
+				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 js-show-modal-search">
 					<i class="zmdi zmdi-search"></i>
-				</div> -->
-			</div>
+				</div>
+			</div> -->
 
 			<!-- Button show menu -->
 			<div class="btn-show-menu-mobile hamburger hamburger--squeeze">
@@ -124,42 +125,39 @@
 					<span class="hamburger-inner"></span>
 				</span>
 			</div>
+		</div>
 
 
-			<!-- Menu Mobile -->
-			<div class="menu-mobile">
-				<ul class="main-menu-m">
-					<li>
-						<a href="index.html">Actividades</a>
-						<span class="arrow-main-menu-m">
-							<i class="fa fa-angle-right" aria-hidden="true"></i>
-						</span>
-					</li>
-
-					<li>
-						<a href="{{ route('blogs.index') }}">Blog</a>
-					</li>
-
+		<!-- Menu Mobile -->
+		<div class="menu-mobile">
+			<ul class="main-menu-m">
+						<li>
+							<a href="{{ route('events.events') }}">Actividades</a>
+						</li>
+						<li>
+							<a href="{{ route('blogs.index') }}">Blog</a>
+						</li>
 					@auth
 						<li>
-							<a href="{{ route('user.profile') }}">Mi Perfil</a>
+							<a href="{{ route('user.profile') }}">Mi perfil</a>
 						</li>
 
 						<li>
 							<a href="{{ route('logout') }}">Cerrar sesión</a>
 						</li>
-
-						@else
+					@else
         				<li>
             				<a href="{{ route('login') }}">Login</a>
         				</li>
         				<li>
             				<a href="{{ route('register') }}">Rexistro</a>
        					 </li>
-       				@endauth
-				</ul>
-			</div>
+       				 @endauth
+					</ul>
+				
+		</div>
 
+		
 			<!-- Modal Search -->
 			<!-- <div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
 			<div class="container-search-header">
@@ -176,93 +174,7 @@
 			</div>
 		</div> -->
 	</header>
-
-	
-
-
-	<!-- <div class="wrap-header-cart js-panel-cart">
-		<div class="s-full js-hide-cart"></div>
-
-		<div class="header-cart flex-col-l p-l-65 p-r-25">
-			<div class="header-cart-title flex-w flex-sb-m p-b-8">
-				<span class="mtext-103 cl2">
-					Your Cart
-				</span>
-
-				<div class="fs-35 lh-10 cl2 p-lr-5 pointer hov-cl1 trans-04 js-hide-cart">
-					<i class="zmdi zmdi-close"></i>
-				</div>
-			</div> -->
-
-	<!-- <div class="header-cart-content flex-w js-pscroll">
-				<ul class="header-cart-wrapitem w-full">
-					<li class="header-cart-item flex-w flex-t m-b-12">
-						<div class="header-cart-item-img">
-							<img src="images/item-cart-01.jpg" alt="IMG">
-						</div>
-
-						<div class="header-cart-item-txt p-t-8">
-							<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-								White Shirt Pleat
-							</a>
-
-							<span class="header-cart-item-info">
-								1 x $19.00
-							</span>
-						</div>
-					</li>
-
-					<li class="header-cart-item flex-w flex-t m-b-12">
-						<div class="header-cart-item-img">
-							<img src="images/item-cart-02.jpg" alt="IMG">
-						</div>
-
-						<div class="header-cart-item-txt p-t-8">
-							<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-								Converse All Star
-							</a>
-
-							<span class="header-cart-item-info">
-								1 x $39.00
-							</span>
-						</div>
-					</li>
-
-					<li class="header-cart-item flex-w flex-t m-b-12">
-						<div class="header-cart-item-img">
-							<img src="images/item-cart-03.jpg" alt="IMG">
-						</div>
-
-						<div class="header-cart-item-txt p-t-8">
-							<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-								Nixon Porter Leather
-							</a>
-
-							<span class="header-cart-item-info">
-								1 x $17.00
-							</span>
-						</div>
-					</li>
-				</ul> -->
-
-	<!-- <div class="w-full">
-					<div class="header-cart-total w-full p-tb-40">
-						Total: $75.00
-					</div>
-
-					<div class="header-cart-buttons flex-w w-full">
-						<a href="shoping-cart.html" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
-							View Cart
-						</a>
-
-						<a href="shoping-cart.html" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-b-10">
-							Check Out
-						</a>
-					</div> -->
-	</div>
-	</div>
-	</div>
-	</div>
+</body>
 
 	@yield('content')
 
